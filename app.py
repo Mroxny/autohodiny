7add5c2f2bf52192f2d6b514995e16180006ee17
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2025-06-02 17:39:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ def main():
 
     try:
         chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--headless=new")
         driver = webdriver.Chrome(options=chrome_options)
         driver.implicitly_wait(10)
